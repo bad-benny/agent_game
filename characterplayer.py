6d3bcf7679033c5650ac1867f182c2a67c49afb0
benn <benny_bad2@outlook.com>
2026-03-21 00:05:01 +0100

    Movement
    Half working movement, very jittery probably caused by my awful input recognition. still proud of myself.
    Could be better, will fix tmrw.

--- characterplayer.py
+++ characterplayer.py
@@ -5,5 +5,16 @@ class Character():
         pass
     
     def drawchar(self, screen, x, y,):
-        pygame.draw.rect(screen, (255, 0, 255), (30, 30, x, y))
+        pygame.draw.rect(screen, (255, 0, 255), (x, y, 30, 30))
     
+    def movechar(self, event, x, y, speed):
+        if event.key == pygame.K_LEFT:
+            x -= speed
+        elif event.key == pygame.K_RIGHT:
+            x += speed
+        elif event.key == pygame.K_UP:
+            y -= speed
+        elif event.key == pygame.K_DOWN:
+            y += speed
+
+        return x, y
--- main.py
+++ main.py
@@ -7,6 +7,10 @@ pygame.display.set_caption('Agent Game')
 screen = pygame.display.set_mode((1080, 920))
 clock = pygame.time.Clock()
 player = Character()
+speed = 5
+
+x = 50
+y = 50
 
 
 running = True
@@ -16,9 +20,13 @@ while running:
 
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            x, y = Character.movechar(player, event, x, y, speed)
 
-    playerdraw = Character.drawchar(player, screen, 50, 50)
-
+    
+    screen.fill((0, 0, 0))
+    
+    playerdraw = Character.drawchar(player, screen, x, y)
 
     pygame.display.update()
 
