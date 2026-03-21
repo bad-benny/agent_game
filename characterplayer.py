d1bf4e7a64946200d600faa88607dcfdc03e057d
benn <benny_bad2@outlook.com>
2026-03-21 09:23:09 +0100

    Movement v.2
    New day new mind, fixed the jittery movement changed a bit values inc. speed, size

--- characterplayer.py
+++ characterplayer.py
@@ -5,16 +5,15 @@ class Character():
         pass
     
     def drawchar(self, screen, x, y,):
-        pygame.draw.rect(screen, (255, 0, 255), (x, y, 30, 30))
+        pygame.draw.rect(screen, (255, 0, 255), (x, y, 50, 50))
     
-    def movechar(self, event, x, y, speed):
-        if event.key == pygame.K_LEFT:
-            x -= speed
-        elif event.key == pygame.K_RIGHT:
-            x += speed
-        elif event.key == pygame.K_UP:
+    def movechar(self, key, x, y, speed):
+        if key[pygame.K_w] or key[pygame.K_UP]:
             y -= speed
-        elif event.key == pygame.K_DOWN:
+        if key[pygame.K_s] or key[pygame.K_DOWN]:
             y += speed
-
+        if key[pygame.K_d] or key[pygame.K_RIGHT]:
+            x += speed
+        if key[pygame.K_a] or key[pygame.K_LEFT]:
+            x -= speed
         return x, y
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ pygame.display.set_caption('Agent Game')
 screen = pygame.display.set_mode((1080, 920))
 clock = pygame.time.Clock()
 player = Character()
-speed = 5
+speed = 1
 
 x = 50
 y = 50
@@ -16,12 +16,15 @@ y = 50
 running = True
 
 while running:
+
+    keys = pygame.key.get_pressed()
+
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            x, y = Character.movechar(player, event, x, y, speed)
+
+    x, y = Character.movechar(player, keys, x, y, speed)
 
     
     screen.fill((0, 0, 0))
